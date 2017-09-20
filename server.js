4af4f901a621df2a33e44f3f5881b611d7a2a902
Nicolas Pieter <nicolas@digitalfolks.sg>
2017-09-21 01:41:40 +0700

    Change name

--- server.js
+++ server.js
@@ -3,3 +3,9 @@ const MongoClient    = require('mongodb').MongoClient;
 const bodyParser     = require('body-parser');
 
 const app            = express();
+const port = 8000;
+
+
+app.listen(port, () => {
+  console.log('We are live on ' + port);
+});

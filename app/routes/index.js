3d1fe928adfbaf1a8b8a6bed43ae87c28b6dbfd6
Nicolas Pieter <nicolas@digitalfolks.sg>
2017-09-23 17:11:36 +0700

    Change to tabs

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,6 @@ const noteRoutes = require('./note_routes');
 
 
 module.exports = function(app, db) {
-  noteRoutes(app, db);
-  // Other route groups could go here, in the future
+	noteRoutes(app, db);
+	// Other route groups could go here, in the future
 };
--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,8 +1,70 @@
+const ObjectID = require('mongodb').ObjectID;
 
 
 module.exports = function( app, db ) {
-	app.post( '/notes', (req,res) => {
-		console.log(req.body)
-		res.send("boi");
+
+
+	app.get( '/notes', (req, res) => {
+		const result = db.collection('notes').find();
+
 	});
+
+
+	app.get( '/notes/:id', (req, res) => {
+		const id = req.params.id;
+		const details = { '_id': new ObjectID(id) };
+		db.collection('notes').findOne(details, (err, item) => {
+			if (err) {
+				res.send({'error':'An error has occurred'});
+			} else {
+				res.send(item);
+			};
+		});
+	});
+
+
+	app.post( '/notes', (req, res) => {
+		const note = {
+			text: req.body.body,
+			title: req.body.title
+		};
+		db.collection('notes').insert(note, (err, result) => {
+			if (err) { 
+				res.send({ 'error': 'An error has occurred' }); 
+			} else {
+				res.send(result.ops[0]);
+			};
+		});
+	});
+
+
+	app.delete( '/notes/:id', (req, res) => {
+		const id = req.params.id;
+		const details = { '_id': new ObjectID(id) };
+		db.collection('notes').remove(details, (err, item) => {
+			if (err) {
+				res.send({'error':'An error has occurred'});
+			} else {
+				res.send('Note' + id + 'deleted!');
+			};
+		});
+	});
+
+
+	app.post('/notes/:id/edit', (req, res) => {
+		const id = req.params.id;
+		const details = { '_id': new ObjectID(id) };
+		const note = {
+			text: req.body.body,
+			title: req.body.title
+		};
+		db.collection('notes').update(details, note, (err, result) => {
+			if (err) {
+				res.send({'error':'An error has occurred'});
+			} else {
+				res.send(note);
+			};
+		});
+	});
+
 };

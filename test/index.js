19a3dd9850c4a0f3578b1c224c0d5bd83d4f221b
Nicolas Pieter <nicolas@digitalfolks.sg>
2017-09-25 01:25:29 +0700

    Changed var

--- test/index.js
+++ test/index.js
@@ -2,11 +2,13 @@ const http = require('http');
 const assert = require('assert');
 
 const config = require('./../src/config');
+const url = 'http://127.0.0.1:' + config.default.port;
+
 require('./../src/server.js');
 
 describe('Example Node Server', () => {
   it('should return 200', done => {
-    http.get('http://127.0.0.1:' + config.default.port, res => {
+    http.get(url, res => {
       assert.equal(200, res.statusCode);
       done();
     });

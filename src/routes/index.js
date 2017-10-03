cdc271ea88f5f69ca79da87b538f18978a9e7132
Nicolas Pieter <nicolas@digitalfolks.sg>
2017-10-03 15:50:26 +0700

    Added route to send swagger file

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,17 @@
 import noteRoutes from './note_routes.js';
-
+import swaggerFile from './../../swagger.json';
 
 export default function ( app, db ) {
 	app.get('/', (req, res) => {
 		res.status(200).send('Hello to node trial');
+	});
+
+	app.get('/swagger.json', (req, res) => {
+		if (swaggerFile) {
+			res.status(200).send(swaggerFile);
+		} else {
+			res.send(404);
+		};
 	});
 
 	noteRoutes(app, db);

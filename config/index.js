4a352c374fc6dfc3d06fcfcefadb8847cb11bc0a
Nicolas Pieter <nicolas@digitalfolks.sg>
2017-10-09 16:59:42 +0800

    Fixed wrong naming

--- config/index.js
+++ config/index.js
@@ -2,4 +2,4 @@ const mongo_conf = require('./mongo.js');
 const mysql_conf = require('./mysql.js');
 const port = require('./port.js');
 
-module.exports = { mongo_conf, mongo_conf, port }
+module.exports = { mongo_conf, mysql_conf, port }

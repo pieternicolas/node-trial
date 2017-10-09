9267570ba90e9fe1e6a7a735d6e52bbeee76bc3b
Nicolas Pieter <nicolas@digitalfolks.sg>
2017-10-09 17:38:01 +0800

    Added validators to user creation

---
+++ src/middleware/validators/user.js
@@ -0,0 +1,18 @@
+const { body } = require('express-validator/check');
+
+
+export default [
+
+	body('firstName')
+		.exists().withMessage('Field is required'),
+	body('lastName')
+		.exists().withMessage('Field is required'),
+	body('username')
+		.exists().withMessage('Field is required'),
+	body('email')
+		.exists().withMessage('Field is required')
+		.isEmail().withMessage('Invalid email format'),
+	body('password')
+		.isLength({ min: 6 }).withMessage('Must be more than 6 characters')
+
+];
--- src/routes/user_routes.js
+++ src/routes/user_routes.js
@@ -1,10 +1,19 @@
 import responder from './../util/responder.js';
 import { userController } from './../controllers/';
+import userValidate from './../middleware/validators/user.js';
+
+const { validationResult } = require('express-validator/check');
 
 
 export default (app) => {
 
-	app.post( '/user', (req, res, next) => {
+	app.post( '/user', userValidate, (req, res, next) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			responder(res, next, { status: 422, data: errors.mapped() });
+			return;
+		};
+
 		userController.createUser(app, req.body)
 			.then(response => {
 				responder(res, next, response);
